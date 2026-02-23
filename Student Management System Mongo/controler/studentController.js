b9a8e0db197a6cf263f0915e4b5268b6549e4217
Dabhi-Pratik <pratikdabhi4902@gmail.com>
2026-02-23 08:34:42 +0530

    update student

--- Student Management System Mongo/controler/studentController.js
+++ Student Management System Mongo/controler/studentController.js
@@ -1,21 +1,95 @@
-import HttpError from "../midware/Httperror.js";
 import StudentModel from "../model/StudentModel.js";
+import HttpError from "../midware/Httperror.js";
 
+
+// ADD STUDENT
 const add = async (req, res, next) => {
-    try{
-        const { firstName, lastName, email, phoneNumber, course, isActive } = req.body
-         
-        const newStudent = { firstName, lastName, email, phoneNumber, course, isActive }
+  try {
+    const student = new StudentModel(req.body);
+    await student.save();
 
-        const studentDetail = await new StudentModel(newStudent)
-         
-        await studentDetail.save();
+    res.status(201).json({
+      message: "Student added successfully",
+      student,
+    });
+  } catch (error) {
+    next(new HttpError(error.message, 500));
+  }
+};
 
-        res.status(201).json({message:"student detail saved",studentDetail})
+
+// GET ALL STUDENTS
+const allStudent = async (req, res, next) => {
+  try {
+    const students = await StudentModel.find();
+
+    res.status(200).json(students);
+  } catch (error) {
+    next(new HttpError(error.message, 500));
+  }
+};
+
+
+// GET BY ID
+const studentById = async (req, res, next) => {
+  try {
+    const student = await StudentModel.findById(req.params.id);
+
+    if (!student) {
+      return next(new HttpError("Student not found", 404));
     }
-    catch(error){
-       next(new HttpError(error.message,500))
-    }
-}
 
-export default add
+    res.status(200).json(student);
+  } catch (error) {
+    next(new HttpError(error.message, 500));
+  }
+};
+
+
+// DELETE
+const deleteStudent = async (req, res, next) => {
+  try {
+    const student = await StudentModel.findByIdAndDelete(req.params.id);
+
+    if (!student) {
+      return next(new HttpError("Student not found", 404));
+    }
+
+    res.status(200).json({ message: "Student deleted successfully" });
+  } catch (error) {
+    next(new HttpError(error.message, 500));
+  }
+};
+
+
+// UPDATE (simple)
+const updateStudent = async (req, res, next) => {
+  try {
+    const updateStudentData = await StudentModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!updateStudentData) {
+      return next(new HttpError("failed to update student data", 404));
+    }
+
+    res.status(200).json({
+      message: "student data updated successfully",
+      updateStudentData,
+    });
+  } catch (error) {
+    next(new HttpError(error.message, 500));
+  }
+};
+
+
+// EXPORT
+export default {
+  add,
+  allStudent,
+  studentById,
+  deleteStudent,
+  updateStudent,
+};
--- Student Management System Mongo/routes/studentRoutes.js
+++ Student Management System Mongo/routes/studentRoutes.js
@@ -1,8 +1,12 @@
-import express from "express"
-import add from "../controler/studentController.js"
+import express from "express";
+import studentController from "../controler/studentController.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/add",add)
+router.post("/add", studentController.add);
+router.get("/all", studentController.allStudent);
+router.get("/:id", studentController.studentById);
+router.delete("/:id", studentController.deleteStudent);
+router.put("/update/:id", studentController.updateStudent);
 
-export default router
+export default router;
