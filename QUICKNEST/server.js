131e834e2d1aa31021f946cc9f055cd189d55b0c
Dabhi-Pratik <pratikdabhi4902@gmail.com>
2026-03-24 21:48:54 +0530

    Basic MVC SetUp in QuickNest

---
+++ QUICKNEST/server.js
@@ -0,0 +1,44 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+import express from "express";
+import connectDB from "./config/db.js";
+import HttpError from "./middleware/HttpError.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res, next) => {
+  res.status(200).json("Hello from Server....!");
+});
+
+app.use((req, res, next) => {
+  return next(new HttpError("Requested Route not Founded...!"));
+});
+
+app.use((error, req, res, next) => {
+  if (res.headersSent) {
+    return next(error);
+  }
+
+  res
+    .status(error.StatusCode || 500)
+    .json(error.message || "Internal Server Error");
+});
+
+const port = process.env.PORT || 5000;
+
+async function startServer() {
+  try {
+    await connectDB();
+
+    app.listen(port, () => {
+      console.log(`Server running on Port ${port}`);
+    });
+  } catch (error) {
+    next(new HttpError(error.message));
+    process.exit(1);
+  }
+}
+
+startServer();
